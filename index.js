6aba9e8f4657bbdd6b06936f5d2af5da7886d327
Sean Nekouei <sean.nekouei@gmail.com>
2022-12-22 20:47:12 -0700

    commit tree 480d902ed07690122a38277a9c91de43dab626cb

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+
+function reverse(s){
+  return s.split("").reverse().join("");
 }
+function isPalindrome(word) { 
+  const reverWord = reverse(word)
+  if(word === reverWord) {
+    return true
+  }
+  else {
+    return false
+  }
+
+}
+  // Write your algorithm here
+
 
 /* 
   Add your pseudocode here
-*/
+* the function isPalindrome with one argument (a string) , 
+itshould return true if the the reverse spell is equal to original 
+otherwise false 
 
 /*
   Add written explanation of your solution here
